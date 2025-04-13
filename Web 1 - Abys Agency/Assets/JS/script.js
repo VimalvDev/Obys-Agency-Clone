a6111144cd30f62719dd05e6b387a4130ec4df5f
Vimal | Frazix <165493811+VimalXfraziX@users.noreply.github.com>
2025-04-13 18:54:33 +0530

    Loader successfully added

--- Web 1 - Abys Agency/Assets/JS/script.js
+++ Web 1 - Abys Agency/Assets/JS/script.js
@@ -1,43 +1,54 @@
-//================loader animation============================
-//up
-gsap.from(".line h1", {
-  yPercent: 100,
-  stagger: 0.3,
-  duration: 0.7,
-  delay: 0.2,
-});
+function loaderAnimation() {
+  let tl = gsap.timeline();
+  //up
+  tl.from(".line h1", {
+    yPercent: 100,
+    stagger: 0.2,
+    duration: 0.7,
+    delay: 0.2,
+  });
+  //timer
+  tl.from(
+    ".timer",
+    {
+      opacity: 0,
+      onStart: function () {
+        let countdown = document.querySelector(".countdown");
+        let count = 0;
+        let timefun = setInterval(() => {
+          if (count <= 100) {
+            countdown.textContent = count++;
+          }
+        }, 25);
+      },
+    },
+    0.6
+  );
+  tl.from(".txt", {
+    opacity: 0,
+  },1);
 
-//timer
-
-gsap.from(".timer", {
-  opacity: 0,
-  delay: 1,
-  onStart: function () {
-    let countdown = document.querySelector(".countdown");
-    let count = 0;
-    let timefun = setInterval(() => {
-      if (count <= 100) {
-        countdown.textContent = count++;
-      }
-    }, 25);
-  },
-});
-//end
-gsap.to(".line span", {
-  opacity: 0,
-  stagger: 0.1,
-  delay: 3.5,
-});
-gsap.to(".line h1", {
-  opacity: 0,
-  stagger: 0.1,
-  delay: 4,
-});
-gsap.to("#loader", {
-  yPercent: -100,
-  duration: 1.5,
-  ease: "power4.out",
-  delay: 4.5,
-});
-
-//nowTxt
+  //end
+  tl.to(
+    ".line span",
+    {
+      opacity: 0,
+      stagger: 0.1,
+    },
+    "+=1.5"
+  );
+  tl.to(
+    ".line h1,.txt",
+    {
+      opacity: 0,
+      stagger: 0.1,
+    },
+    "-=0.5"
+  );
+  tl.to("#loader", {
+    yPercent: -100,
+    duration: 1.5,
+    ease: "power4.out",
+  });
+}
+loaderAnimation();
